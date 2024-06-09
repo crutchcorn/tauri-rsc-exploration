e7e07bf302c35eff121aae071183c8edbda26295
Corbin Crutchley <git@crutchcorn.dev>
2024-06-09 02:24:13 -0700

    chore: add RSC demo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
-export default function Home() {
-  return (
-    <main>
-      <div>Hello world!</div>
-    </main>
-  );
+export default async function Home() {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    return (
+        <main>
+            <div>Hello world!</div>
+        </main>
+    );
 }
